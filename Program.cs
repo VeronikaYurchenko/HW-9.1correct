236d579c5926f2b5a341470d1e716eafd29678d0
Yurchenko Veronika <vasilieva-nika@yandex.ru>
2022-09-24 19:30:48 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+string GetRecursion(int m, int n)
+{
+if (m == n) return Convert.ToString(n);
+return m + ", " + GetRecursion(m + 1, n);
+}
+ System.Console.WriteLine(GetRecursion (1, 5));
+
